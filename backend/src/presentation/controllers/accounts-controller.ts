b1c8c6f86a368d618f6bba0ea51482eba4288063
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-11-17 11:43:09 -0300

    :bug: fix: renaming returns

--- backend/src/presentation/controllers/accounts-controller.ts
+++ backend/src/presentation/controllers/accounts-controller.ts
@@ -2,7 +2,9 @@ import { Request, Response } from "express";
 
 export class AccountsController {
   public async getBalance(_: Request, res: Response) {
-    try { } catch (err) {
+    try { 
+      return res.status(200)
+     } catch (err) {
       return res.status(400).json({
         message: 'Could not get balance',
         error: err.message
--- backend/src/presentation/controllers/transactions-controller.ts
+++ backend/src/presentation/controllers/transactions-controller.ts
@@ -2,18 +2,22 @@ import { Request, Response } from "express";
 
 export class TransactionsController {
   public async list(_: Request, res: Response) {
-    try { } catch (err) {
+    try { 
+      return res.status(200)
+    } catch (err) {
       return res.status(400).json({
-        message: 'Could list transactions',
+        message: 'Could not list transactions',
         error: err.message
       })
     }
   }
 
   public async cashOut(req: Request, res: Response) {
-    try { } catch (err) {
+    try { 
+      return res.status(201)
+    } catch (err) {
       return res.status(400).json({
-        message: 'Could do this transaction',
+        message: 'Could not do this transaction',
         error: err.message
       })
     }
