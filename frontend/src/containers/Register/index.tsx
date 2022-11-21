c26fec02f2cb4933c6cc2e3990db4e7c99433c90
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-11-21 17:06:09 -0300

    :lipstick: feat: register interface

--- frontend/src/containers/Register/index.tsx
+++ frontend/src/containers/Register/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, FormContainer, StyledTextField, SubmitButton, LoginRedirect } from './styles';
 
 const Register: React.FC = () => {
   return (
     <Container>
-      Register
+      <FormContainer>
+        <h1>REGISTRATION</h1>
+        <StyledTextField id="outlined-basic" label="Username" variant="outlined" />
+        <StyledTextField id="outlined-basic" label="Password" variant="outlined" />
+        <SubmitButton>Register</SubmitButton>
+        <LoginRedirect>
+          Already a member?
+          <a href="/login">Login here</a>
+        </LoginRedirect>
+      </FormContainer>
     </Container>
   );
-}
+};
 
 export default Register;
