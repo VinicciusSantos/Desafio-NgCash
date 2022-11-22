f82dd869de665469a21b1a4e2c9ec82f2471c201
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-11-22 09:19:42 -0300

    :sparkles: feat: login form

--- frontend/src/containers/Login/index.tsx
+++ frontend/src/containers/Login/index.tsx
@@ -1,13 +1,93 @@
-import React from 'react';
+import React, { SyntheticEvent, useState } from 'react';
+import api from '../../api';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
-import { Container } from './styles';
+import {
+  Container,
+  FormContainer,
+  StyledTextField,
+  SubmitButton,
+  LoginRedirect,
+} from './styles';
 
 const Login: React.FC = () => {
+  const [snackbarInfos, setSnackbarInfos] = useState({
+    isOpen: false,
+    message: '',
+  });
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (event: any) => {
+    setLoginData({
+      ...loginData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onSubmit = async (e: Event) => {
+    e.preventDefault();
+    api
+      .post('/auth/login', loginData)
+      .then((res) => console.log(res))
+      .catch((err) => {
+        setSnackbarInfos({
+          isOpen: true,
+          message: err.response.data.error,
+        });
+      });
+  };
+
+  const handleClose = (event?: SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') return;
+    setSnackbarInfos({
+      isOpen: false,
+      message: '',
+    });
+  };
+
   return (
     <Container>
-      Login
+      <FormContainer>
+        <h1>LOGIN</h1>
+        <StyledTextField
+          id="outlined-basic"
+          name="username"
+          label="Username"
+          value={loginData.username}
+          onChange={(e) => handleChange(e)}
+          variant="outlined"
+        />
+        <StyledTextField
+          id="outlined-basic"
+          name="password"
+          label="Password"
+          value={loginData.password}
+          onChange={(e) => handleChange(e)}
+          variant="outlined"
+        />
+        <SubmitButton onClick={(e: any) => onSubmit(e)}>Login</SubmitButton>
+        <LoginRedirect>
+          Not a member?
+          <a href="/login">Signup now</a>
+        </LoginRedirect>
+      </FormContainer>
+
+      <Snackbar
+        open={snackbarInfos.isOpen}
+        autoHideDuration={4000}
+        anchorOrigin={{ "horizontal": "left", "vertical": "top"}}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} variant="filled" severity="error" sx={{ width: '100%' }}>
+          {snackbarInfos.message}
+        </Alert>
+      </Snackbar>
     </Container>
   );
-}
+};
 
 export default Login;
--- frontend/src/containers/Login/styles.ts
+++ frontend/src/containers/Login/styles.ts
@@ -1,5 +1,101 @@
+import TextField from '@mui/material/TextField';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+    90deg,
+    #f9d923,
+    #00a19d,
+    #0c87b7,
+    #cb49ff,
+    #ff6666,
+    #f9d923
+  );
+  background-size: 400%;
+  animation: glow 25s linear infinite;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  &:after {
+    background-image: url('../assets/swirl.png');
+    position: absolute;
+    top: 0;
+    z-index: 0;
+    content: '';
+    width: 100%;
+    height: 100%;
+    opacity: .5;
+  }
+`;
+
+export const FormContainer = styled.form`
+  z-index: 1;
+  background-color: #222;
+  color: #eee;
+  border-radius: 10px;
+  padding: 10px;
+  height: auto;
+  padding: 25px 0;
+  width: 400px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledTextField = styled(TextField)`
+  width: 90%;
+  margin: 5px 0 !important;
+
+  .MuiInputBase-root {
+      background-color: #1c1c1c !important;
+  }
+
+  * {
+    border-color: #00000010 !important;
+    color: #ccc !important;
+  }
+`;
+
+export const SubmitButton = styled.button`
+  border-color: transparent;
+  border-radius: 5px;
+  background: linear-gradient(
+    90deg,
+    #f9d923,
+    #00a19d,
+    #0c87b7,
+    #cb49ff,
+    #ff6666,
+    #f9d923
+  );
+  background-size: 400%;
+  animation: glow 25s linear infinite;
+  width: 90%;
+  padding: 10px 0;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+
+export const LoginRedirect = styled.div`
+    font-size: 13px;
+    width: 90%;
+    margin-top: 8px;
+    text-align: end;
+
+    a {
+        margin-left: 5px;
+        text-decoration: none;
+        color: #00a19d;
+        transition-duration: .3s;
+        
+        &:hover {
+            transition-duration: .3s;
+            color: #00c990;
+        }
+    }
 `;
