362e98b97b1f38fdbf147d3c9cfed9ffd69ef3c2
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-11-25 10:34:00 -0300

    :bricks: ci: setup docker

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,10 @@
 import { AppDataSource } from "./data/data-sources/type-orm-data-source";
 import server from "./server";
 
-const port = process.env.PORT || '3030'
+const port = process.env.PORT || '5000'
 
 console.log("Starting server...");
 AppDataSource.initialize().then(() => {
-  console.log(`Server is running`);
+  console.log(`Server is running in port ${port}`);
   server.listen(port)
 });
