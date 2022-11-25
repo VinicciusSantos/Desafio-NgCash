dbb35c0433006d48cbb345f1894aad1334672223
VinicciusSantos <comercial.viniciusguedes@gmail.com>
2022-11-25 13:15:44 -0300

    :bug: fix: ajusting console port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,8 @@
 import { AppDataSource } from "./data/data-sources/type-orm-data-source";
 import server from "./server";
 
-const port = process.env.PORT || '5000'
-
 console.log("Starting server...");
 AppDataSource.initialize().then(() => {
-  console.log(`Server is running in port ${port}`);
-  server.listen(port)
+  console.log(`Server is running in port :3050`);
+  server.listen(5000)
 });
